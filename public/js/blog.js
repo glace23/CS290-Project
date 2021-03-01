1baa8a8ca75dd1dc0bf5f35a72bfb2a5dedc99f1
glace23 <vikash23galce@gmail.com>
2021-02-28 20:23:33 -0800

    change server

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,20 +1,20 @@
 function redirect() {
-	window.location = "http://localhost:30244/password"
+	window.location = "http://flip3.engr.oregonstate.edu:32044/password"
 }
 
 function passwordRedirect(){
 	var password =  document.getElementById('password').value
 	// if password is correct redirect to write blog
 	if (password == "1234"){
-		location = "http://localhost:30244/writeBlog"
+		location = "http://flip3.engr.oregonstate.edu:32044/writeBlog"
 	}
 	// go back to blog
 	else{
-		location = "http://localhost:30244/blog"
+		location = "http://flip3.engr.oregonstate.edu:32044/blog"
 	}
 	return false
 }
 
 function gotoblogRedirect(){
-	window.location = "http://localhost:30244/blog"
+	window.location = "http://flip3.engr.oregonstate.edu:32044/blog"
 }
--- public/js/comment.js
+++ public/js/comment.js
@@ -3,7 +3,7 @@ function redirect() {
 	var comment = document.getElementById('comment').value
 	// if username or comment is empty redirect to contact page
 	if (username == "" || comment == ""){
-        location = 'http://localhost:30244/contact';
+        location = 'http://flip3.engr.oregonstate.edu:32044/contact';
 	}
     else{
     	return true;
